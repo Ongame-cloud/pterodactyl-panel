e00ed494f967e33d2503b2098a638948ca9acbf9
Ongamecloud <girard.mathis@outlook.com>
2025-11-14 16:17:40 -1000

    Force storage permissions with find command

--- railway-start.sh
+++ railway-start.sh
@@ -12,6 +12,8 @@ fi
 
 mkdir -p storage/logs storage/framework/sessions storage/framework/views storage/framework/cache/data bootstrap/cache
 chmod -R 777 storage bootstrap/cache
+find storage -type d -exec chmod 777 {} \;
+find storage -type f -exec chmod 666 {} \;
 
 if [ -z "$APP_KEY" ] || [ "$APP_KEY" = "" ]; then
     echo "Generating APP_KEY..."
